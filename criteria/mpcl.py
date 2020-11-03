c21981c28c8c81409e0ea138aac4752d98b6fd64
Soroosh Shahtalebi <30849241+shahtalebi@users.noreply.github.com>
2020-11-03 01:39:29 -0500

    Update mpcl.py
    log based loss is added

--- criteria/mpcl.py
+++ criteria/mpcl.py
@@ -26,24 +26,60 @@ class Criterion(torch.nn.Module):
         self.REQUIRES_OPTIM      = REQUIRES_OPTIM
 
 
-    def forward(self, batch, labels, **kwargs):
+    # def forward(self, batch, labels, **kwargs):
+        # anchors, positives, negatives = self.batchminer(batch, labels)
+
+        # ##
+        # loss  = 0
+        # if 'bninception' in self.pars.arch:
+            # ### clamping/value reduction to avoid initial overflow for high embedding dimensions!
+            # batch = batch/4
+        # for anchor, positive, negative_set in zip(anchors, positives, negatives):
+            # a_embs, p_embs, n_embs = batch[anchor:anchor+1], batch[positive:positive+1], batch[negative_set]
+            # cos1 = torch.nn.CosineSimilarity(dim=1)
+            # cp = cos1(a_embs,p_embs)
+            # # cn = torch.zeros(a_embs.shape[0])
+            # cn = cos1(a_embs,n_embs[0, None,:])
+            # for k in range(1,n_embs.shape[0]):
+                # cn += cos1(a_embs,n_embs[k, None,:])
+            # cn = cn / n_embs.shape[0]
+            # loss = (cp - cn - 1).pow(2)
+            # loss = loss[:,None]
+
+        # return loss
+
+	def forward(self, batch, labels, **kwargs):
         anchors, positives, negatives = self.batchminer(batch, labels)
 
         ##
         loss  = 0
-        if 'bninception' in self.pars.arch:
+        #if 'bninception' in self.pars.arch:
             ### clamping/value reduction to avoid initial overflow for high embedding dimensions!
-            batch = batch/4
+            #batch = batch/4
         for anchor, positive, negative_set in zip(anchors, positives, negatives):
             a_embs, p_embs, n_embs = batch[anchor:anchor+1], batch[positive:positive+1], batch[negative_set]
             cos1 = torch.nn.CosineSimilarity(dim=1)
             cp = cos1(a_embs,p_embs)
             # cn = torch.zeros(a_embs.shape[0])
             cn = cos1(a_embs,n_embs[0, None,:])
+            #for k in range(1,n_embs.shape[0]):
+            #    temp = cos1(a_embs,n_embs[k, None,:])
+            #    if cn < temp:
+            #        cn = temp
+            #eps = 1e-5
+            #loss = (cp - cn - 1).pow(2).clamp(0,1.0-eps)
+            
+            
+            
             for k in range(1,n_embs.shape[0]):
                 cn += cos1(a_embs,n_embs[k, None,:])
             cn = cn / n_embs.shape[0]
-            loss = (cp - cn - 1).pow(2)
+            eps = 1e-7
+            loss = (cp - cn - 1).pow(2).clamp(0,1.0-eps)
+            
+            
+            
+            loss = - torch.log(-loss + 1)
             loss = loss[:,None]
 
         return loss
